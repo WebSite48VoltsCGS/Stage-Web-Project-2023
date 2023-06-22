ef1350338f997a7bf3aa2e9afe74a063d203b7fb
NightFore <Et3rnNighTaLL@Hotmail.com>
2023-06-22 11:58:10 +0200

    Day 9 - Added hidden password

--- coventGardenStudios/coventGarden/studios/forms.py
+++ coventGardenStudios/coventGarden/studios/forms.py
@@ -1,18 +1,26 @@
 from django import forms
 
 # Global variables
-LENGTH_USERNAME = 256
+LENGTH_NAME = 256
 LENGTH_PASSWORD = 256
 LENGTH_EMAIL = 320
 
+# Fields
+FIELD_USERNAME = forms.CharField(max_length=LENGTH_NAME, label="Nom d'utilisateur")
+FIELD_EMAIL = forms.EmailField(max_length=LENGTH_EMAIL, label="Adresse e-mail")
+FIELD_PASSWORD = forms.CharField(max_length=LENGTH_PASSWORD, label="Mot de passe", widget=forms.PasswordInput)
+FIELD_CONFIRM = forms.CharField(max_length=LENGTH_PASSWORD, label="Confirmer le mot de passe", widget=forms.PasswordInput)
+FIELD_FIRST_NAME = forms.CharField(max_length=LENGTH_NAME, label="Prénom")
+FIELD_LAST_NAME = forms.CharField(max_length=LENGTH_NAME, label="Nom")
+
 # Register your forms here
 class SignUpForm(forms.Form):
-    username = forms.CharField(max_length=LENGTH_USERNAME, label="Nom d'utilisateur")
-    first_name = forms.CharField(max_length=LENGTH_USERNAME, label="Prénom")
-    last_name = forms.CharField(max_length=LENGTH_USERNAME, label="Nom")
-    email = forms.EmailField(max_length=LENGTH_EMAIL, label="Adresse e-mail")
-    password = forms.CharField(max_length=LENGTH_PASSWORD, label="Mot de passe")
-    confirm_password = forms.CharField(max_length=LENGTH_PASSWORD, label="Confirmer le mot de passe")
+    username = FIELD_USERNAME
+    email = FIELD_EMAIL
+    first_name = FIELD_FIRST_NAME
+    last_name = FIELD_LAST_NAME
+    password = FIELD_PASSWORD
+    confirm_password = FIELD_CONFIRM
 
     # Class = 'form-control'
     def __init__(self, *args, **kwargs):
@@ -21,8 +29,8 @@ class SignUpForm(forms.Form):
             visible.field.widget.attrs['class'] = 'form-control'
 
 class SignInForm(forms.Form):
-    username = forms.CharField(max_length=LENGTH_USERNAME, label="Nom d'utilisateur")
-    password = forms.CharField(max_length=LENGTH_PASSWORD, label="Mot de passe")
+    username = FIELD_USERNAME
+    password = FIELD_PASSWORD
 
     # Class = 'form-control'
     def __init__(self, *args, **kwargs):
@@ -31,7 +39,7 @@ class SignInForm(forms.Form):
             visible.field.widget.attrs['class'] = 'form-control'
 
 class PasswordResetForm(forms.Form):
-    email = forms.EmailField(max_length=LENGTH_EMAIL, label="Adresse e-mail")
+    email = FIELD_EMAIL
 
     # Class = 'form-control'
     def __init__(self, *args, **kwargs):
